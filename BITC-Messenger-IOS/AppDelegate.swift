d2e1aa34b51e571e4814bd0b02c45b7ae1115c72
Unknown <kalter_z@mail.ru>
2018-05-24 12:13:23 +0200

    Merge branch 'Artem'

--- BITC-Messenger-IOS/AppDelegate.swift
+++ BITC-Messenger-IOS/AppDelegate.swift
@@ -75,6 +75,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }()
 
     // MARK: - Core Data Saving support
+    // MARK: - Core Data Saving support
+    // MARK: - Core Data Saving support
+    // MARK: - Core Data Saving support
     
     func saveContext () {
         let context = persistentContainer.viewContext
