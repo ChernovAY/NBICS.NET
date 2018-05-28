3af4985d48a3f59ff0d29b85041a3c3fa36fead5
Unknown <kalter_z@mail.ru>
2018-05-28 15:10:19 +0200

    New

---
+++ BITC-Messenger-IOS/Chats.swift
@@ -0,0 +1,11 @@
+//
+//  Chats.swift
+//  BITC-Messenger-IOS
+//
+//  Created by Анна Гринер on 28.05.2018.
+//  Copyright © 2018 riktus. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
---
+++ BITC-Messenger-IOS/Conversations.swift
@@ -0,0 +1,12 @@
+//
+//  Conversations.swift
+//  BITC-Messenger-IOS
+//
+//  Created by Анна Гринер on 28.05.2018.
+//  Copyright © 2018 riktus. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+
