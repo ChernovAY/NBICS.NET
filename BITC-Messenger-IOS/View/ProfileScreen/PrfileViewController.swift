c96f7fff580695cce32d742c6dd2e656e8133ae0
Unknown <kalter_z@mail.ru>
2018-06-05 17:47:51 +0200

    0.3

---
+++ BITC-Messenger-IOS/View/ProfileScreen/PrfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PrfileViewController.swift
+//  BITC-Messenger-IOS
+//
+//  Created by bender on 05.06.2018.
+//  Copyright © 2018 riktus. All rights reserved.
+//
+
+import UIKit
+
+class PrfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
